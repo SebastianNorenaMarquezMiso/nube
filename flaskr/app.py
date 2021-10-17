1646e3134b7d723cb912dab2a9d1cf04f50e85b8
javargas1098 <ja.vargas1098@gmail.com>
2021-10-17 14:43:56 -0500

    Arreglos

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,24 +1,26 @@
 from flask import Flask
+# from .app import create_app
 from flask_restful import Api
 #from .modelos import db
-from vistas import VistaFiles
+# from .vistas import VistaFiles
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS, cross_origin
+from vistas import VistaFiles
 
-
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = 'uploaded'
+DOWNLOAD_FOLDER = 'download'
 
 
 def create_app(config_name):
     app = Flask(__name__)
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sbbzmftwcpqmbs:63d510222da8ee4b0f412e7b34cd5b203ef12907f963453b33dec8f9f2169a0d@ec2-18-215-44-132.compute-1.amazonaws.com/d5h5dmuoe660fa'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
     return app
-
 
 app = create_app('default')
 app_context = app.app_context()
