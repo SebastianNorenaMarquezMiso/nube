d06d8021716d71d3768cedcaa363e61f638eb6a6
Sebastián Noreña Márquez <senorena@condorlabs.io>
2021-10-23 16:36:51 -0500

    test

--- conversion/flaskr/tareas/tareas.py
+++ conversion/flaskr/tareas/tareas.py
@@ -6,31 +6,35 @@ from celery import Celery
 from celery.signals import task_postrun
 from flask import current_app
 
-from ..app import db
-from ..modelos import Task, TaskSchema
+from app import db
+from modelos import Task, TaskSchema
 
 task_schema = TaskSchema()
 
 app = Celery('tasks', broker='redis://redis:6379/0')
-
+print("****")
+print(app)
 
 @app.task(name="tabla.file_conversion")
 def file_conversion(request_json):
     # Build input path and add file
+    print("1")
     inputF = os.path.join(
         os.path.dirname(__file__).replace("tareas", "") + current_app.config['UPLOAD_FOLDER'],
         request_json["filename"])
-
+    print("2")
     # Build output path and add file
     outputF = os.path.join(
         os.path.dirname(__file__).replace("tareas", "") + current_app.config['DOWNLOAD_FOLDER'], request_json["dfile"])
-
+ 
     # Ffmpeg is flexible enough to handle wildstar conversions
     # convertCMD = ['ffmpeg', '-y', '-i', inputF, outputF]
     # convertCMD = ['/usr/bin/ffmpeg', '-y', '-i', inputF, outputF]
+    print("3")
     convertCMD = ['ffmpeg', '-y', '-i', inputF, outputF]
+    print("4")
     executeOrder66 = sp.Popen(convertCMD)
-
+    print("5")
     try:
         outs, errs = executeOrder66.communicate(
             timeout=10)  # tell program to wait
