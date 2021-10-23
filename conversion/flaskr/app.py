32875cb108e96b1e9e36eb746e26b62e933daeb7
javargas1098 <ja.vargas1098@gmail.com>
2021-10-23 10:41:58 -0500

    Arreglos

--- conversion/flaskr/app.py
+++ conversion/flaskr/app.py
@@ -1,9 +1,26 @@
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
-from flaskr import create_app
+# from flaskr import create_app
 from .modelos import db
 from .vistas import VistaFiles, VistaUpdateFiles, VistaDeleteFiles, VistaGetFiles
+from flask import Flask
+from celery import Celery
+
+UPLOAD_FOLDER = 'uploaded'
+DOWNLOAD_FOLDER = 'download'
+
+celery = Celery(__name__, broker='redis://localhost:6379/0')
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://jimmy.orjuela:admin@localhost:5432/test"
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    app.config['JWT_SECRET_KEY']='frase-secreta'
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    return app
 
 app = create_app('default')
 app_context = app.app_context()
