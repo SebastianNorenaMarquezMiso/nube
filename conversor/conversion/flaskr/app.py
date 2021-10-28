b02295421dd86a4fd676541324ef83b1b825be2d
Sebastián Noreña Márquez <senorena@condorlabs.io>
2021-10-27 22:41:27 -0500

    config

--- conversor/conversion/flaskr/app.py
+++ conversor/conversion/flaskr/app.py
@@ -40,4 +40,4 @@ api.add_resource(VistaTest, '/test')
 jwt = JWTManager(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True , port=5001)
+    app.run(host='0.0.0.0', debug=True , port=80)
