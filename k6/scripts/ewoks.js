e49484718766dfc2543546f079ba440bde802e21
Sebastián Noreña Márquez <senorena@condorlabs.io>
2021-11-07 11:00:03 -0500

    change url

--- k6/scripts/ewoks.js
+++ k6/scripts/ewoks.js
@@ -4,7 +4,7 @@ import { check, sleep } from "k6";
 export let options = {
   stages: [
       // Ramp-up from 1 to 5 virtual users (VUs) in 5s
-      { duration: "5s", target: 5 , vue:2 },
+      { duration: "1m", target: 200 , vue:5 },
 
      /* // Stay at rest on 5 VUs for 10s
       { duration: "10s", target: 5 },
@@ -27,9 +27,9 @@ export default function () {
   var data = {
     'file': http.file(binFile, 'test.mp3'),
     'newFormat': 'wma',
-};
-let params =  { headers: { "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNTEwMzM5MCwianRpIjoiYzIxOGYwZDMtY2UzZC00NDFmLWI1M2EtODkwMzBlMTMzMzQ1IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjM1MTAzMzkwLCJleHAiOjE2MzUxMDQyOTB9.kHZZBDYiwYN5tDkU6OB2sPFWDOMzJeUK1mqBLp9c_Jk" } }
-var response = http.post('http://172.19.0.1:5000/api/tasks', data,params);
+  };
+  let params =  { headers: { "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNjMwMDU5NCwianRpIjoiNDVkNTBmOGYtNWViOS00MDY1LTlkNGItNWY2ZjAyODRkMmY0IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjM2MzAwNTk0LCJleHAiOjE2MzYzMDE0OTR9.xB-xAlBQRHdWKnKhTsankiPEk5Ngvh7GzeAQDrHdWUA" } }
+  var response = http.post('http://ec2-44-198-165-161.compute-1.amazonaws.com/api/tasks', data,params);
 
   check(response, { "status is 200": (r) => r.status === 200 });
   console.log(response.status);
