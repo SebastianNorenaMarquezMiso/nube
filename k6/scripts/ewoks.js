f2ad5330e9f2f7e53c0704ac1dad4a54872bc5fc
Sebastián Noreña Márquez <senorena@condorlabs.io>
2021-11-07 12:35:18 -0500

    change config

--- k6/scripts/ewoks.js
+++ k6/scripts/ewoks.js
@@ -3,20 +3,25 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-      // Ramp-up from 1 to 5 virtual users (VUs) in 5s
-      { duration: "1m", target: 200 , vue:5 },
-
-     /* // Stay at rest on 5 VUs for 10s
-      { duration: "10s", target: 5 },
-
-      // Ramp-down from 5 to 0 VUs for 5s
-      { duration: "5s", target: 0 }*/
+      { duration: "1m", target: 1 , vue:1 }
   ]
 };
 let binFile = open('./rauw.mp3', 'b');
 export default function () {
-  
-  //console.log(binFile);
+  var data = {
+    'file': http.file(binFile, 'test.mp3'),
+    'newFormat': 'wma',
+  };
+  let params =  { headers: { "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNjMwNjAzMywianRpIjoiNzgwODFiMmUtMzc0Yy00NGIzLWE2N2UtMTQzMTQxMWEzMGI0IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjM2MzA2MDMzLCJleHAiOjE2MzYzMDY5MzN9.cQQVxTbK5-1GK-C26lBE2SAExeAJuZXbzlVodsZ-sEc" } }
+  var response = http.get('http://ec2-18-207-99-175.compute-1.amazonaws.com/api/tasks', data,params);
+
+  check(response, { "status is 200": (r) => r.status === 200 });
+  console.log(response.status);
+  sleep(.300);
+};
+
+
+//console.log(binFile);
   //const response = http.get("https://swapi.dev/api/people/30/", {headers: {Accepts: "application/json"}});
   //check(response, { "status is 200": (r) => r.status === 200 });
   
@@ -24,17 +29,5 @@ export default function () {
 
   //const response = http.get("https://swapi.dev/api/people/30/", {headers: {Accepts: "application/json"}});
   
-  var data = {
-    'file': http.file(binFile, 'test.mp3'),
-    'newFormat': 'wma',
-  };
-  let params =  { headers: { "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNjMwMDU5NCwianRpIjoiNDVkNTBmOGYtNWViOS00MDY1LTlkNGItNWY2ZjAyODRkMmY0IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjM2MzAwNTk0LCJleHAiOjE2MzYzMDE0OTR9.xB-xAlBQRHdWKnKhTsankiPEk5Ngvh7GzeAQDrHdWUA" } }
-  var response = http.post('http://ec2-44-198-165-161.compute-1.amazonaws.com/api/tasks', data,params);
-
-  check(response, { "status is 200": (r) => r.status === 200 });
-  console.log(response.status);
-  sleep(.300);
-};
-
 
 
