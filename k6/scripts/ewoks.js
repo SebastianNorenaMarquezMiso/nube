78df9fd3fec8e2028fe7094952c8cca89f9156ec
Sebastián Noreña Márquez <senorena@condorlabs.io>
2021-11-10 23:54:23 -0500

    script de configuracion

--- k6/scripts/ewoks.js
+++ k6/scripts/ewoks.js
@@ -3,7 +3,7 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-      { duration: "1m", target: 1 , vue:1 }
+      { duration: "1m", target: 2 , vue:30 }
   ]
 };
 let binFile = open('./rauw.mp3', 'b');
@@ -12,8 +12,8 @@ export default function () {
     'file': http.file(binFile, 'test.mp3'),
     'newFormat': 'wma',
   };
-  let params =  { headers: { "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNjMwNjAzMywianRpIjoiNzgwODFiMmUtMzc0Yy00NGIzLWE2N2UtMTQzMTQxMWEzMGI0IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjM2MzA2MDMzLCJleHAiOjE2MzYzMDY5MzN9.cQQVxTbK5-1GK-C26lBE2SAExeAJuZXbzlVodsZ-sEc" } }
-  var response = http.get('http://ec2-18-207-99-175.compute-1.amazonaws.com/api/tasks', data,params);
+  let params =  { headers: { "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzNjMwNjczNCwianRpIjoiMmQzM2NkZGQtYjE0ZS00OGNhLWI4ZjYtNzQ3YmYzMzNjNTRjIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjM2MzA2NzM0LCJleHAiOjE2MzYzMDc2MzR9.gpr8AHee3ncrs5-Lvas-rjGqJhEuH16bnoaZtVNk7gY" } }
+  var response = http.post('http://ec2-18-207-99-175.compute-1.amazonaws.com/api/tasks', data,params);
 
   check(response, { "status is 200": (r) => r.status === 200 });
   console.log(response.status);
